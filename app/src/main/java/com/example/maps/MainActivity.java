7eeacb4cc5a79b18b0650bdf617d64cded38a9f4
batteashishkumar <ashishakash55@gmail.com>
2019-08-27 17:36:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.maps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void maps(View v){
+        startActivity(new Intent(this,MapsActivity.class));
+
+    }
+}
